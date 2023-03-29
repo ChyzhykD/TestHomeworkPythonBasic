1003f2b940aacc102cda3fd9c1f86d195dca6c0c
ChyzhykD <main.dch296@gmail.com>
2023-03-29 16:20:53 +0300

    Робота над ДЗ_1 1620 2903

--- Homework_1.py
+++ Homework_1.py
@@ -6,3 +6,7 @@ STR_1 = "Hello"
 print(STR_1, int_1 - int_2)
 list_1 = [int_1, int_2, int_3, STR_1]
 print(set(list_1))
+tuple_1 = (-5, 6, 2.7, "tuple_")
+tuple_2 = ("WORLD", 555, -4.1, -8)
+tuple_3 = tuple_1 + tuple_2
+print(list(tuple_3))
