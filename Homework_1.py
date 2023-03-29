5584d725ed7818839c4b804d16388ace6883f0b1
ChyzhykD <main.dch296@gmail.com>
2023-03-29 21:27:26 +0300

    Робота над ДЗ_1 1634 2903

--- Homework_1.py
+++ Homework_1.py
@@ -1,10 +1,10 @@
 int_1 = 8
 int_2 = 7
-int_3 = (int_1 == int_2)
-print(int_3)
+bool_1 = int_1 == int_2
+print(bool_1)
 STR_1 = "Hello"
-print(STR_1, int_1 - int_2)
-list_1 = [int_1, int_2, int_3, STR_1]
+print(STR_1 + str(int_1 - int_2))
+list_1 = [int_1, int_2, bool_1, STR_1]
 print(set(list_1))
 tuple_1 = (-5, 6, 2.7, "tuple_")
 tuple_2 = ("WORLD", 555, -4.1, -8)
