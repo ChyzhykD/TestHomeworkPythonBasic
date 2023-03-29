96c165d37a78f4a3355b2fd9c34c0edfa137974a
ChyzhykD <main.dch296@gmail.com>
2023-03-29 16:28:17 +0300

    Робота над ДЗ_1 1628 2903

--- Homework_1.py
+++ Homework_1.py
@@ -10,3 +10,4 @@ tuple_1 = (-5, 6, 2.7, "tuple_")
 tuple_2 = ("WORLD", 555, -4.1, -8)
 tuple_3 = tuple_1 + tuple_2
 print(list(tuple_3))
+dict_1 = {'Ali': 18, 'Bo': 'Mar', 20: 'NewY', False: None, True: ['y', 1], 32.1: {'A': 'big', 1: 8}, (1, 2): (3, 4)}
