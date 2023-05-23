ccf3d0693a2d44c000afab077a8ccb6aed669df6
ChyzhykD <129075837+ChyzhykD@users.noreply.github.com>
2023-05-23 12:02:38 +0300

    Update Homework_6.py

--- Homework_6.py
+++ Homework_6.py
@@ -1,8 +1,31 @@
-import random
-import string
-import time
-print(time.time())
-print(string.ascii_letters)
-time.sleep(4)
-print(random.choice(string.ascii_letters))
-print(time.time())
+# import random
+# import string
+# import time
+
+
+def sum_of_list_items(arr):
+    total_sum = 0
+    for item in arr:
+        if isinstance(item, int):
+            total_sum += item
+        elif isinstance(item, list):
+            total_sum += sum_of_list_items(item)
+    return total_sum
+
+
+def cycle_words(words, output_length):
+    cycled_words = []
+    item_index = 0
+    while len(cycled_words) < output_length:
+        if item_index < len(words):
+            cycled_words += words[item_index]
+            item_index += 1
+        else:
+            item_index = 0
+
+
+# check the function:
+if __name__ == '__main__':
+    assert sum_of_list_items([]) == 0
+    assert sum_of_list_items([1, 2]) == 3
+    assert sum_of_list_items([1, [2, 3, [4], [5, 6, [7]]]]) == 28
