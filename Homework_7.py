1e7afd548179b83549fcd403a83ca92d354acb48
ChyzhykD <129075837+ChyzhykD@users.noreply.github.com>
2023-05-23 15:51:46 +0300

    Update Homework_7.py

--- Homework_7.py
+++ Homework_7.py
@@ -13,12 +13,31 @@ def retry(attempts=5, desired_value=None):
     return decorator
 
 
-# get_random_value()
-# get_random_value_with_default_attempts()
-# get_random_values_with_default_attempts([1, 2, 3, 4])
-# get_random_values_with_default_attempts([1, 2, 3, 4], 2)
-# get_random_values_with_default_attempts([1, 2, 3, 4], size=2)
-# get_random_values_with_default_attempts(choices=[1, 2, 3, 4], size=2)
-# get_random_values([1, 2, 3, 4])
-# get_random_values([1, 2, 3, 4], 3)
-# get_random_values([1, 2, 3, 4], size=1)
+@retry(desired_value=3)
+def get_random_value_with_default_attempts():
+    return random.choice((1, 2, 3, 4, 5))
+
+
+@retry(desired_value=[1, 2])
+def get_random_values_with_default_attempts(choices, size=2):
+    return random.choices(choices, k=size)
+
+
+@retry(attempts=7, desired_value=3)
+def get_random_value():
+    return random.choice((1, 2, 3, 4, 5))
+
+
+@retry(attempts=2, desired_value=[1, 2, 3])
+def get_random_values(choices, size=2):
+    return random.choices(choices, k=size)
+
+
+get_random_value_with_default_attempts()
+get_random_values_with_default_attempts([1, 2, 3, 4])
+get_random_values_with_default_attempts([1, 2, 3, 4], 2)
+get_random_values_with_default_attempts([1, 2, 3, 4], size=2)
+get_random_values_with_default_attempts(choices=[1, 2, 3, 4], size=2)
+get_random_values([1, 2, 3, 4])
+get_random_values([1, 2, 3, 4], 3)
+get_random_values([1, 2, 3, 4], size=1)
